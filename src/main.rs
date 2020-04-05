47f1e5a66170a271cb4eb86a7978004be10e34da
adeyahya <adeyahyaprasetyo@gmail.com>
2020-04-06 06:00:33 +0700

    persist data into sqllite

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use rusqlite::{params, Connection, Result};
 #[macro_use] extern crate prettytable;
 use prettytable::{Table, Row, Cell};
 
+#[derive(Debug)]
 struct Todo {
     id: i16,
     title: String,
@@ -9,8 +11,10 @@ struct Todo {
     deleted: bool,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let mut todos: Vec<Todo> = Vec::new();
+    let conn = Connection::open("db.db")?;
+    load_data(&conn, &mut todos)?;
 
     loop {
         let mut commands = String::new();
@@ -32,15 +36,15 @@ fn main() {
             },
             _ => {
                 match command_parts[0] {
-                    "add" => add_todo(&mut todos, &command_parts[1..].join(" ")),
+                    "add" => add_todo(&conn, &mut todos, &command_parts[1..].join(" "))?,
                     "remove" => {
                         if let Ok(num) = command_parts[1].parse::<i16>() {
-                            remove_todo(&mut todos, num);
+                            remove_todo(&conn, &mut todos, num)?;
                         }
                     },
                     "done" => {
                         if let Ok(num) = command_parts[1].parse::<i16>() {
-                            mark_done(&mut todos, num);
+                            mark_done(&conn, &mut todos, num)?;
                         }
                     },
                     _ => invalid_command(&commands),
@@ -50,21 +54,53 @@ fn main() {
     }
 }
 
-fn mark_done(todos: &mut Vec<Todo>, num: i16) {
+fn load_data(conn: &Connection, todos: &mut Vec<Todo>) -> Result<()> {
+    conn.execute("CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        completed BOOLEAN,
+        deleted BOOLEAN
+    )", params![])?;
+
+    let mut data = conn.prepare("SELECT * from todos where deleted = false")?;
+
+    let todos_iter = data.query_map(params![], |row| {
+        Ok(Todo {
+            id: row.get(0)?,
+            title: row.get(1)?,
+            completed: row.get(2)?,
+            deleted: row.get(3)?
+        })
+    })?;
+
+    for todo in todos_iter {
+        todos.push(todo.unwrap());
+    }
+    Ok(())
+}
+
+fn mark_done(conn: &Connection, todos: &mut Vec<Todo>, num: i16) -> Result<()> {
     if let Some(todo) = todos.iter_mut().find(|todo| todo.id == num) {
         todo.completed = true;
+
+        conn.execute("update todos set completed = true where id = ?1", params![num])?;
     }
+
     print_todos(&todos);
+    Ok(())
 }
 
-fn remove_todo(todos: &mut Vec<Todo>, num: i16) {
+fn remove_todo(conn: &Connection, todos: &mut Vec<Todo>, num: i16) -> Result<()> {
     if let Some(todo) = todos.iter_mut().find(|todo| todo.id == num) {
         todo.deleted = true;
+
+        conn.execute("update todos set deleted = true where id = ?1", params![num])?;
     }
     print_todos(&todos);
+    Ok(())
 }
 
-fn add_todo(todos: &mut Vec<Todo>, title: &str) {
+fn add_todo(conn: &Connection, todos: &mut Vec<Todo>, title: &str) -> Result<()> {
     let new_id = todos.len() as i16 + 1;
 
     todos.push(Todo {
@@ -74,7 +110,15 @@ fn add_todo(todos: &mut Vec<Todo>, title: &str) {
         deleted: false,
     });
 
+    conn.execute(
+        "INSERT INTO TODOS
+            (id, title, completed, deleted)
+            values (?1, ?2, ?3, ?4)",
+        params![new_id, title.to_string(), false, false]
+    )?;
+
     print_todos(&todos);
+    Ok(())
 }
 
 fn print_todos(todos: &Vec<Todo>) {
