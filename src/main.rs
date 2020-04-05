c5237706b7690684878df3dc4ee1c44d67839722
adeyahya <adeyahyaprasetyo@gmail.com>
2020-04-06 06:10:28 +0700

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
 use rusqlite::{params, Connection, Result};
-#[macro_use] extern crate prettytable;
-use prettytable::{Table, Row, Cell};
+use std::io;
+#[macro_use]
+extern crate prettytable;
+use prettytable::{Cell, Row, Table};
 
 #[derive(Debug)]
 struct Todo {
@@ -18,49 +19,48 @@ fn main() -> Result<()> {
 
     loop {
         let mut commands = String::new();
-        
+
         println!("command: ");
         io::stdin()
             .read_line(&mut commands)
             .expect("failed to read input");
 
         let command_parts: Vec<&str> = commands.split_whitespace().collect();
-        
+
         match command_parts.len() {
             0 => invalid_command(&commands),
-            1 => {
-                match command_parts[0] {
-                    "list" => print_todos(&todos),
-                    _ => invalid_command(&commands)
-                }
+            1 => match command_parts[0] {
+                "list" => print_todos(&todos),
+                _ => invalid_command(&commands),
             },
-            _ => {
-                match command_parts[0] {
-                    "add" => add_todo(&conn, &mut todos, &command_parts[1..].join(" "))?,
-                    "remove" => {
-                        if let Ok(num) = command_parts[1].parse::<i16>() {
-                            remove_todo(&conn, &mut todos, num)?;
-                        }
-                    },
-                    "done" => {
-                        if let Ok(num) = command_parts[1].parse::<i16>() {
-                            mark_done(&conn, &mut todos, num)?;
-                        }
-                    },
-                    _ => invalid_command(&commands),
+            _ => match command_parts[0] {
+                "add" => add_todo(&conn, &mut todos, &command_parts[1..].join(" "))?,
+                "remove" => {
+                    if let Ok(num) = command_parts[1].parse::<i16>() {
+                        remove_todo(&conn, &mut todos, num)?;
+                    }
                 }
-            }
+                "done" => {
+                    if let Ok(num) = command_parts[1].parse::<i16>() {
+                        mark_done(&conn, &mut todos, num)?;
+                    }
+                }
+                _ => invalid_command(&commands),
+            },
         }
     }
 }
 
 fn load_data(conn: &Connection, todos: &mut Vec<Todo>) -> Result<()> {
-    conn.execute("CREATE TABLE IF NOT EXISTS todos (
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todos (
         id INTEGER PRIMARY KEY,
         title TEXT NOT NULL,
         completed BOOLEAN,
         deleted BOOLEAN
-    )", params![])?;
+    )",
+        params![],
+    )?;
 
     let mut data = conn.prepare("SELECT * from todos where deleted = false")?;
 
@@ -69,7 +69,7 @@ fn load_data(conn: &Connection, todos: &mut Vec<Todo>) -> Result<()> {
             id: row.get(0)?,
             title: row.get(1)?,
             completed: row.get(2)?,
-            deleted: row.get(3)?
+            deleted: row.get(3)?,
         })
     })?;
 
@@ -83,7 +83,10 @@ fn mark_done(conn: &Connection, todos: &mut Vec<Todo>, num: i16) -> Result<()> {
     if let Some(todo) = todos.iter_mut().find(|todo| todo.id == num) {
         todo.completed = true;
 
-        conn.execute("update todos set completed = true where id = ?1", params![num])?;
+        conn.execute(
+            "update todos set completed = true where id = ?1",
+            params![num],
+        )?;
     }
 
     print_todos(&todos);
@@ -94,7 +97,10 @@ fn remove_todo(conn: &Connection, todos: &mut Vec<Todo>, num: i16) -> Result<()>
     if let Some(todo) = todos.iter_mut().find(|todo| todo.id == num) {
         todo.deleted = true;
 
-        conn.execute("update todos set deleted = true where id = ?1", params![num])?;
+        conn.execute(
+            "update todos set deleted = true where id = ?1",
+            params![num],
+        )?;
     }
     print_todos(&todos);
     Ok(())
@@ -114,7 +120,7 @@ fn add_todo(conn: &Connection, todos: &mut Vec<Todo>, title: &str) -> Result<()>
         "INSERT INTO TODOS
             (id, title, completed, deleted)
             values (?1, ?2, ?3, ?4)",
-        params![new_id, title.to_string(), false, false]
+        params![new_id, title.to_string(), false, false],
     )?;
 
     print_todos(&todos);
@@ -132,7 +138,7 @@ fn print_todos(todos: &Vec<Todo>) {
             table.add_row(Row::new(vec![
                 Cell::new(done),
                 Cell::new(&todo.id.to_string()),
-                Cell::new(&todo.title.to_string())
+                Cell::new(&todo.title.to_string()),
             ]));
         }
     }
